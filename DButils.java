ae973042a844413d6a7a0f7fb562710fce2bbd2d
N Sreenivasulu reddy <seenureddy564@gmail.com>
2021-09-13 13:13:51 +0530

    first commit

--- DButils.java
+++
@@ -1,5 +0,0 @@
-i am devloping this line in dev  branch
-updating file today at 11 am isd
-updating this line at 10:24 isd
-hello again i am updating file
-hello guys this java class
---
+++ seenu.py
@@ -0,0 +1,2 @@
+hi guys this is my commit on vsc
+now i am started learn devops tools
